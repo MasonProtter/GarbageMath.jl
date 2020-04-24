997c258ec7dfeb63c84123ebb535a556ad0c392f
MasonProtter <mason.protter@icloud.com>
2020-04-23 19:58:41 -0600

    inital commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# GarbageMath.jl
+
+A julia implementation of [garbage math](https://xkcd.com/2295/). Look out, [Measurements.jl](https://github.com/JuliaPhysics/Measurements.jl)
+
+```julia
+julia> using GarbageMath
+
+julia> Precise(1.2) + Precise(3.5)
+Slightly less precise number 4.7
+
+julia> Precise(3) * Precise(1)
+Slightly less precise number 3
+
+julia> Precise(1) + Garbage(2)
+Garbage number 3
+
+julia> Precise(1e10) * Garbage(1e-6)
+Garbage number 10000.0
+
+julia> √(Garbage(5))
+Less bad garbage number 2.23606797749979
+
+julia> Garbage(6)^2
+Worse garbage number 36
+
+julia> mean(StatisticallyIndependant([Garbage(3), Garbage(4), Garbage(5)]))
+Better garbage number 4.0
+
+julia> Precise(5)^Garbage(6)
+Much worse garbage number 15625
+
+julia> Garbage(2) - Garbage(10)
+Much worse garbage number -8
+
+julia> Precise(10)/(Garbage(3) - Garbage(4))
+Much worse garbage number -10.0
+
+julia> Garbage(3) * 0
+Precise number 0
+```
---
+++ src/GarbageMath.jl
@@ -0,0 +1,65 @@
+module GarbageMath
+
+export XKCDNumber, Precise, Garbage, mean, StatisticallyIndependant
+
+using Statistics: Statistics, mean
+
+struct XKCDNumber{Badness, T} <: Number 
+    n::T 
+end
+XKCDNumber{Badness}(x) where {Badness} = XKCDNumber{Badness, typeof(x)}(x)
+
+const Precise = XKCDNumber{1}
+const Garbage = XKCDNumber{10}
+
+get_badness(::XKCDNumber{Badness}) where {Badness} = Badness
+
+function Base.show(io::IO, x::XKCDNumber{n}) where {n}
+    qualifier = if n <= 0.25
+        "Very precise"
+    elseif n <= 0.5
+        "More precise"
+    elseif n <= 1
+        "Precise"
+    elseif n <= 2
+        "Slightly less precise"
+    elseif n <= 3
+        "Less precise"
+    elseif n < 5
+        "Better garbage"
+    elseif n < 7 
+        "Less bad garbage"
+    elseif n < 15
+        "Garbage"
+    elseif n < 25 
+        "Worse garbage"
+    else
+        "Much worse garbage"
+    end
+    print(io, qualifier, " number ", x.n)
+end
+
+
+struct StatisticallyIndependant
+    xs::Vector
+end
+
++̂(x, y) = maximum((x + y, 0.0))
+
+Base.:(+)(x::XKCDNumber{N}, y::XKCDNumber{M}) where {N, M} = XKCDNumber{N +̂ M}(x.n + y.n)
+Base.:(*)(x::XKCDNumber{N}, y::XKCDNumber{M}) where {N, M} = XKCDNumber{N +̂ M}(x.n * y.n)
+
+Base.:(√)(x::XKCDNumber{N}) where {N} = XKCDNumber{N/2}(√(x.n))
+Base.literal_pow(::typeof(^), x::XKCDNumber{N}, ::Val{2}) where {N} = XKCDNumber{2N}(x.n ^ 2)
+
+Statistics.mean(v::StatisticallyIndependant) = XKCDNumber{mean(get_badness.(v.xs))/3}(mean((x -> x.n).(v.xs)))
+
+Base.:(^)(x::XKCDNumber{N}, y::XKCDNumber{M}) where {N, M} = XKCDNumber{4*(N +̂ M)}(x.n ^ y.n)
+Base.:(-)(x::XKCDNumber{N}, y::XKCDNumber{M}) where {N, M} = XKCDNumber{2*(N +̂ M)}(x.n - y.n)
+
+Base.:(/)(x::XKCDNumber{N}, y::XKCDNumber{M}) where {N, M} = XKCDNumber{100*(N +̂ M)}(x.n / y.n)
+
+Base.:(*)(x::XKCDNumber{N}, y::Number)  where {N} = y == 0 ? Precise(0) : XKCDNumber{N + 1}(x.n * y)
+Base.:(*)(x::Number, y::XKCDNumber) = y * x
+
+end
